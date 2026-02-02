f7bc6e64c8d7cd682b3d80bf10b97102ab97d15b
Jacob <Jacobdhartzell@gmail.com>
2026-02-02 12:10:52 -0500

    edits

---
+++ js02.js
@@ -0,0 +1,18 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Jacob
+      Date:   1/30/26
+
+      Filename: js02.js
+ */
+
+// set the form's default values
+document.getElementById("photoNum").value = 1;
+document.getElementById("photoHrs").value = 2;
+document.getElementById("makeBook").checked = false;
+document.getElementById("photoRights").checked = false;
+document.getElementById("photoDist").value = 0;
